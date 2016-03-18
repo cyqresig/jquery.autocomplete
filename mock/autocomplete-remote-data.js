510b3af26a1e9d21895dca59d42c827e3abbd356
cyqresig <moonsunfall@aliyun.com>
2016-03-18 13:48:13 +0800

    调整了模拟数据, 以支持特定的搜索词

--- mock/autocomplete-remote-data.js
+++ mock/autocomplete-remote-data.js
@@ -7,11 +7,12 @@
 
 var Mock = require('mockjs');
 
-var remoteJSONData = Mock.mock( /\/sug/, {
+//%E6%B7%98 -> 淘
+var remoteJSONData_tao = Mock.mock( /\/sug\?searchKey=%E6%B7%98$/, {
 
 	// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
 	'list|1-10': [{
-		'keyword': '淘宝宝' + '@ctitle'
+		'keyword': '淘' + '@ctitle'
 	}]
 });
 //$.ajax({
@@ -22,5 +23,25 @@ var remoteJSONData = Mock.mock( /\/sug/, {
 //		.appendTo('body')
 //})
 
+//%E6%B7%98%E5%AE%9D -> 淘宝
+var remoteJSONData_taobao = Mock.mock( /\/sug\?searchKey=%E6%B7%98%E5%AE%9D$/, {
 
-module.exports = remoteJSONData;
+	// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+	'list|1-10': [{
+		'keyword': '淘宝' + '@ctitle'
+	}]
+});
+
+var remoteJSONData_taobaobao = Mock.mock( /\/sug\?searchKey=%E6%B7%98%E5%AE%9D%E5%AE%9D$/, {
+
+	// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+	'list|1-10': [{
+		'keyword': '淘宝宝' + '@ctitle'
+	}]
+});
+
+exports.remoteJSONData_tao = remoteJSONData_tao;
+exports.remoteJSONData_taobao = remoteJSONData_taobao;
+exports.remoteJSONData_taobaobao = remoteJSONData_taobaobao;
+
+
